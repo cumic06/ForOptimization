cdf7a604f69accce8fa35142d9c548ef754ef5a2
cumic <changhwan060224@gmail.com>
2024-07-27 00:56:25 +0900

    ForOptimization
    for문 최적화

---
+++ ForOptimization/ForOptimization/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace ForOptimization
+{
+    class Program
+    {
+        static void Main()
+        {
+            For();
+            ForOptimization();
+        }
+
+        private static void For()
+        {
+            int realRunCount = 0;
+            int workRunCount = 0;
+            for (realRunCount = 1, workRunCount = 0; workRunCount < 100; realRunCount++)
+            {
+                realRunCount++;
+                workRunCount++;
+            }
+            Console.WriteLine($"{realRunCount} {workRunCount} {100}");
+        }
+
+        private static void ForOptimization()
+        {
+            int realRunCount = 0;
+            int workRunCount = 0;
+            for (realRunCount = 1, workRunCount = 0; workRunCount < 100; realRunCount++)
+            {
+                realRunCount++;
+                workRunCount++;
+
+                realRunCount++;
+                workRunCount++;
+
+                realRunCount++;
+                workRunCount++;
+
+                realRunCount++;
+                workRunCount++;
+
+                realRunCount++;
+                workRunCount++;
+
+                realRunCount++;
+                workRunCount++;
+
+                realRunCount++;
+                workRunCount++;
+
+                realRunCount++;
+                workRunCount++;
+
+                realRunCount++;
+                workRunCount++;
+
+                realRunCount++;
+                workRunCount++;
+            }
+            Console.WriteLine($"{realRunCount} {workRunCount} {100}");
+        }
+    }
+}
